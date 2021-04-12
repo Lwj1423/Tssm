3c2f61bedf7e99023ae5f7e717d2c319d31e453d
Lwj1423 <m17621954542@163.com>
2021-04-12 17:23:06 +0800

    单例模式

--- src/main/java/JavaBase/designMode/SingletonTest.java
+++ src/main/java/JavaBase/designMode/SingletonTest.java
@@ -7,10 +7,17 @@ public class SingletonTest {
 
     public static void main(String[] args) {
 
+        //饿汉
         Bank bank1 = Bank.getInstance();
         Bank bank2 = Bank.getInstance();
 
         System.out.println(bank1 == bank2);
+
+        //懒汉
+        Order order1 = Order.getInstance();
+        Order order2 = Order.getInstance();
+
+        System.out.println(order1 == order2);
     }
 
 }
@@ -33,4 +40,27 @@ class Bank{
     public static Bank getInstance(){
         return instance;
     }
+}
+
+/**
+ * 懒汉式
+ */
+class Order{
+
+    //私有化类的构造器
+    private Order(){
+
+    }
+
+    private static Order instance = null;
+
+    //创建公共静态方法
+    public static Order getInstance(){
+        if (instance == null){
+            instance = new Order();
+        }
+
+        return instance;
+    }
+
 }
