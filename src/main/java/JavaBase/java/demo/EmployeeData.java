37c7c13c05f8178c2f81f8a33a5f153bc65407cc
Lwj1423 <m17621954542@163.com>
2021-05-12 08:38:34 +0800

    测试01

---
+++ src/main/java/JavaBase/java/demo/EmployeeData.java
@@ -0,0 +1,25 @@
+package JavaBase.java.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 提供测试的数据
+ */
+public class EmployeeData {
+
+    public static List<Employee> getEmployees(){
+        List<Employee> list = new ArrayList<>();
+
+        list.add(new Employee(1001,"aa",34,7000));
+        list.add(new Employee(1002,"bb",10,6000));
+        list.add(new Employee(1003,"cc",20,5000));
+        list.add(new Employee(1004,"dd",30,4000));
+        list.add(new Employee(1005,"ee",40,3000));
+        list.add(new Employee(1006,"ff",33,2000));
+        list.add(new Employee(1007,"gg",22,1000));
+
+
+        return list;
+    }
+}
