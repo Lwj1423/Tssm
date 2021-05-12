fbd211791b9f435626a252d76d754055f7303209
Lwj1423 <m17621954542@163.com>
2021-05-12 10:10:13 +0800

    测试01

--- src/main/java/JavaBase/java/demo/Employee.java
+++ src/main/java/JavaBase/java/demo/Employee.java
@@ -46,6 +46,12 @@ public class Employee {
     public Employee() {
     }
 
+    public Employee(int id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
     public Employee(int id, String name, int age, double salary) {
         super();
         this.id = id;
@@ -69,5 +75,30 @@ public class Employee {
                 age + "\t\t" +
                 salary
                 ;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (id != employee.id) return false;
+        if (age != employee.age) return false;
+        if (Double.compare(employee.salary, salary) != 0) return false;
+        return name != null ? name.equals(employee.name) : employee.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + age;
+        temp = Double.doubleToLongBits(salary);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 }
--- src/main/java/JavaBase/java/demo/EmployeeData.java
+++ src/main/java/JavaBase/java/demo/EmployeeData.java
@@ -12,11 +12,11 @@ public class EmployeeData {
         List<Employee> list = new ArrayList<>();
 
         list.add(new Employee(1001,"aa",34,7000));
-        list.add(new Employee(1002,"bb",10,6000));
+        list.add(new Employee(1002,"bbbb",10,6000));
         list.add(new Employee(1003,"cc",20,5000));
         list.add(new Employee(1004,"dd",30,4000));
         list.add(new Employee(1005,"ee",40,3000));
-        list.add(new Employee(1006,"ff",33,2000));
+        list.add(new Employee(1006,"ffff",33,2000));
         list.add(new Employee(1007,"gg",22,1000));
 
 
